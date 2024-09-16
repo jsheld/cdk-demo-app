b31cf128ef04d5e8b42f6d23e14ec8e843822600
Jonathan Held <jsheld@amazon.com>
2024-09-16 12:59:04 -0500

    Initial checkin#

--- lib/cdk2-stack.ts
+++ lib/cdk2-stack.ts
@@ -1,4 +1,5 @@
 import * as cdk from 'aws-cdk-lib';
+import { Bucket, BucketEncryption } from 'aws-cdk-lib/aws-s3';
 import { Construct } from 'constructs';
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
@@ -12,5 +13,13 @@ export class Cdk2Stack extends cdk.Stack {
     // const queue = new sqs.Queue(this, 'Cdk2Queue', {
     //   visibilityTimeout: cdk.Duration.seconds(300)
     // });
+
+    const bucket = new Bucket(this, "mycdkbucket", {
+      bucketName: "myfirstbucketcdk-2024-09-16",
+      removalPolicy: cdk.RemovalPolicy.RETAIN,
+      encryption: BucketEncryption.KMS_MANAGED,
+      versioned: true
+    })
+
   }
 }
